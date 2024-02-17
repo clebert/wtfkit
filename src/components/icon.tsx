fb8464d4da5509ceccea1cd0b699fba76085c785
Clemens Akens <clebert@me.com>
2024-02-17 14:16:07 +0100

    Improve icon component props interface

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import { joinClassNames } from '../utils/join-class-names.js';
 
-export function Icon({ className, svgClassName, type, standalone }: IconProps): JSX.Element {
+export function Icon({ className, type, standalone }: IconProps): JSX.Element {
   return (
-    <div
-      className={joinClassNames(className, `inline-flex h-5 align-middle`, !standalone && `mr-1`)}
-    >
-      {React.createElement(type, { className: joinClassNames(svgClassName, `h-4 w-4`) })}
+    <div className={joinClassNames(`inline-flex h-5 align-middle`, !standalone && `mr-1`)}>
+      {React.createElement(type, { className: joinClassNames(className, `h-4 w-4`) })}
     </div>
   );
 }
 
 export interface IconProps {
   className?: string | undefined;
-  svgClassName?: string | undefined;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   type: React.FunctionComponent<any> | React.ComponentClass<any>;
   standalone?: boolean | undefined;
